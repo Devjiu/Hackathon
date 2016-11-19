44056df977108e7e5fc6e3c553990138c323a7a5
Petr Kurapov <petr.kurapov@gmail.com>
2016-11-19 20:37:26 +0300

    Login routines

--- server/views.py
+++ server/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+import json
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        received_data = json.load(request.POST['login','password'])
+
+        return HttpResponse(str(received_data))
+
+     return Http404
